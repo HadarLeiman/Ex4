fc6d1c23303f7b575f87c7c12699317a1abdbf03
oneOfThePeople <gogogta@gmail.com>
2022-12-28 17:14:14 +0200

    3

--- cmake-build-debug/CMakeFiles/Ex3.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Ex3.dir/DependInfo.cmake
@@ -8,6 +8,11 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/אני/CLionProjects/Ex3/DataProcessor.cpp" "CMakeFiles/Ex3.dir/DataProcessor.cpp.o" "gcc" "CMakeFiles/Ex3.dir/DataProcessor.cpp.o.d"
+  "/mnt/c/Users/אני/CLionProjects/Ex3/KNeighborsClassifier.cpp" "CMakeFiles/Ex3.dir/KNeighborsClassifier.cpp.o" "gcc" "CMakeFiles/Ex3.dir/KNeighborsClassifier.cpp.o.d"
+  "/mnt/c/Users/אני/CLionProjects/Ex3/Server.cpp" "CMakeFiles/Ex3.dir/Server.cpp.o" "gcc" "CMakeFiles/Ex3.dir/Server.cpp.o.d"
+  "/mnt/c/Users/אני/CLionProjects/Ex3/distance_functions.cpp" "CMakeFiles/Ex3.dir/distance_functions.cpp.o" "gcc" "CMakeFiles/Ex3.dir/distance_functions.cpp.o.d"
+  "/mnt/c/Users/אני/CLionProjects/Ex3/input_validation.cpp" "CMakeFiles/Ex3.dir/input_validation.cpp.o" "gcc" "CMakeFiles/Ex3.dir/input_validation.cpp.o.d"
   "/mnt/c/Users/אני/CLionProjects/Ex3/main.cpp" "CMakeFiles/Ex3.dir/main.cpp.o" "gcc" "CMakeFiles/Ex3.dir/main.cpp.o.d"
   )
 
